712412e983e1c223af0c5f45e2cb26400e4743d0
ianjsikora <ianjsikora@gmail.com>
2015-03-02 10:46:37 -0800

    Mar 02, 2014: Final Blink Sync

--- blink/urls.py
+++ blink/urls.py
@@ -2,18 +2,23 @@ from django.conf.urls import url, include
 from rest_framework import routers, filters
 from blink_app import views
 from blink_app.views import ContentViewSet
+from django.contrib import admin
 
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'content', views.ContentViewSet)
+# router.register(r'test', views.SearchTest)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
 
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'results', views.SearchViewSet.as_view(),),
     url(r'^', include(router.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'results', views.SearchViewSet.as_view(),),
+    url(r'year', views.YearSearchViewSet.as_view(),),
+    url(r'genre', views.GenreSearchViewSet.as_view(),),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
 ]
